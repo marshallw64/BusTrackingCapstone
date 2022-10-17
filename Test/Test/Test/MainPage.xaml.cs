b69e16700c03c38c34411ffb7e34af1eaa1763eb
marshallw64 <78442083+marshallw64@users.noreply.github.com>
2022-10-17 14:39:38 -0500

    Marshall's Lesson Code
    This is from the first part of the Xamarin lesson.

---
+++ Test/Test/Test/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            string username = nameEntry.Text;
+            string greeting = "Hello " + username;
+            greetingLabel.Text = greeting;
+        }
+    }
+}
+
